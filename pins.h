8809d8c46ce94c9d6231e9f8cee1351e6de88312
Sean - <sean.seah0311@gmail.com>
2022-01-14 22:39:38 +0800

    push

--- pins.h
+++ pins.h
@@ -3,49 +3,32 @@
 #define _PINS_HPP_
 
 //FL Motor
-#define PWM_FL 2
-#define FWD_FL 23
-#define BWD_FL 25
+#define PWM_FL 9
+#define FWD_FL 52
+#define BWD_FL 50
 
 //FR Motor 
-#define PWM_FR 6
-#define FWD_FR 26
-#define BWD_FR 28
+#define PWM_FR 8
+#define FWD_FR 48
+#define BWD_FR 46
 
 //BR Motor
-#define PWM_BR 9
-#define FWD_BR 30
-#define BWD_BR 32
+#define PWM_BR 6
+#define FWD_BR 53
+#define BWD_BR 51
 
 //BL Motor
-#define PWM_BL 8
-#define FWD_BL 35
-#define BWD_BL 37
-
-//MR Motor
-#define PWM_MR 5
-#define FWD_MR 22
-#define BWD_MR 24
-
-//ML Motor
-#define PWM_ML 3
-#define FWD_ML 27
-#define BWD_ML 29
+#define PWM_BL 7
+#define FWD_BL 49
+#define BWD_BL 47
 
 //Roller Motor
-#define PWM_ROLLER_L 7
-#define FWD_ROLLER_L 31
-#define BWD_ROLLER_L 33
+#define PWM_ROLLER_L 10
+#define FWD_ROLLER_L 23
+#define BWD_ROLLER_L 25
 
-#define PWM_ROLLER_R 10
-#define FWD_ROLLER_R 34
-#define BWD_ROLLER_R 36
+#define PWM_ROLLER_R 11
+#define FWD_ROLLER_R 22
+#define BWD_ROLLER_R 24
 
-//Intake/Indexer
-#define PWM_INTAKE 11
-#define FWD_INTAKE 50
-#define BWD_INTAKE 51
-#define PWM_INDEX 12
-#define FWD_INDEX 52
-#define BWD_INDEX 53
 #endif
--- roboclash.ino
+++ roboclash.ino
@@ -1,31 +1,67 @@
-#include <PPMReader.h>
+#include <Servo.h>
+#include <IBusBM.h>
 #include "pins.h"
 
-int maxSpeed = 250;
-//baseline lowest 5 speed is 50
-byte interruptPin = 3;
-byte channelAmount = 8;
-PPMReader ppm(interruptPin, channelAmount);
+////////////////////////////////////////////MOTORS/////////////////////////////////////
+class Motor {
+  private:
+    int PWM, FWD, BWD;
+    bool reversed;
+    int maxSpeed;
 
-struct Motor {
-  const int PWM;
-  const int FWD;
-  const int BWD;
-  const bool reversed;
-  const int maxSpeed;
+  public:
+    Motor(int PWM, int FWD, int BWD, bool reversed, int maxSpeed){
+      this->PWM = PWM;
+      this->FWD = FWD;
+      this->BWD = BWD;
+      this->reversed = reversed;
+      this->maxSpeed = maxSpeed;
+    }
+
+    void init(){
+      pinMode(this->PWM,OUTPUT);
+      pinMode(this->FWD,OUTPUT);
+      pinMode(this->BWD,OUTPUT);
+    }
+
+    void move(int speed){
+      if (speed >= this->maxSpeed) speed = this->maxSpeed;
+      if (speed <= -this->maxSpeed) speed = -this->maxSpeed;
+      analogWrite(this->PWM, abs(speed)); 
+      if (this->reversed) {
+        digitalWrite(this->FWD, speed < 0);
+        digitalWrite(this->BWD, speed > 0);
+      } else {
+        digitalWrite(this->FWD, speed > 0);
+        digitalWrite(this->BWD, speed < 0);
+      }
+    }
 };
 
-int readChannel (int channelNumber, PPMReader ppm) {
-  unsigned value = ppm.latestValidChannelValue(channelNumber, 0);
-  return value;
+////////////////////////////////////////////CONTROLS/////////////////////////////////////
+IBusBM ibusRc;
+HardwareSerial& ibusRcSerial = Serial1;
+
+void setup_ibus() {
+  ibusRc.begin(ibusRcSerial);
 }
 
-int controllerMap(int value) {
-  //map value from -255 to 255
+int readChannel(int channelNumber) {
+  return ibusRc.readChannel(channelNumber);
+}
+
+int readSwitch(int channelNumber) {
+  int value = ibusRc.readChannel(channelNumber);
+  if (value == 1000) return 1;
+  else if (value == 2000) return 0;  
+  return -1; //refers to err
+}
+
+int controllerMap(int value, int maxSpeed) {
   if (value >= 1000 && value < 1450) {
-    value = map(value,1000,1450,-255,-50);
+    value = map(value,1000,1450,-maxSpeed,-50);
   } else if (value <= 2000 && value > 1550) {
-    value = map(value,1550,2000,50,255);
+    value = map(value,1550,2000,50,maxSpeed);
   } else {
     //assume stick is stationary
     value = 0;
@@ -33,62 +69,82 @@ int controllerMap(int value) {
   return value;
 }
 
-void setupMotor (Motor motor) {
-  pinMode(motor.FWD, OUTPUT);
-  pinMode(motor.BWD, OUTPUT);
-  pinMode(motor.PWM, OUTPUT);
+////////////////////////////////////////////BASE LIB/////////////////////////////////////
+int motorMaxSpeed = 250; //motor Max Speed
+Motor FL (PWM_FL, FWD_FL, BWD_FL, false, motorMaxSpeed);
+Motor FR (PWM_FR, FWD_FR, BWD_FR, true, motorMaxSpeed);
+Motor BL (PWM_BL, FWD_BL, BWD_BL, true, motorMaxSpeed);
+Motor BR (PWM_BR, FWD_BR, BWD_BR, true, motorMaxSpeed);
+Motor Roller_L (PWM_ROLLER_L, FWD_ROLLER_L, BWD_ROLLER_L, false, motorMaxSpeed);
+Motor Roller_R (PWM_ROLLER_R, FWD_ROLLER_R, BWD_ROLLER_R, true, motorMaxSpeed);
+
+void setup_motors()
+{
+  FL.init();
+  BL.init();
+  FR.init();
+  BR.init();
+  Roller_R.init();
+  Roller_L.init();
 }
 
-//note speed is from -255 to 255
-void moveMotor (Motor motor, int speed) {
-  if (speed >= motor.maxSpeed)speed=motor.maxSpeed;
-  if (speed <= -motor.maxSpeed)speed=-motor.maxSpeed;
-  if (motor.reversed) {
-    digitalWrite(motor.FWD, speed < 0);
-    digitalWrite(motor.BWD, speed > 0);
-  } else {
-    digitalWrite(motor.FWD, speed > 0);
-    digitalWrite(motor.BWD, speed < 0);
-  }
-  analogWrite(motor.PWM, abs(speed));
+void powerBase(double l, double r) {
+  FL.move(l);
+  FR.move(r);
+  BL.move(l);
+  BR.move(r);
 }
+
+void roller(double power) {
+  Roller_L.move(power);
+  Roller_R.move(power);
+}
+
+////////////////////////////////////////////MAIN/////////////////////////////////////
+Servo indexer;
 
 void setup() {
   Serial.begin(115200);
-  Serial.print("Exiting Setup");
+  setup_motors();
+  setup_ibus();
+  indexer.attach(27);
+  indexer.write(180);
 }
 
 void loop() {
-  Motor FL = {PWM_FL,FWD_FL,BWD_FL,true,maxSpeed}; 
-  Motor FR = {PWM_FR,FWD_FR,BWD_FR,false,maxSpeed};
-  Motor ML = {PWM_ML,FWD_ML,BWD_ML,true,maxSpeed};
-  Motor MR = {PWM_MR,FWD_MR,BWD_MR,false,maxSpeed};
-  Motor BR = {PWM_BR,FWD_BR,BWD_BR,false,maxSpeed};
-  Motor BL = {PWM_BL,FWD_BL,BWD_BL,false,maxSpeed};
-  Motor RollerL = {PWM_ROLLER_L, FWD_ROLLER_L, BWD_ROLLER_L, false, maxSpeed};
-  Motor RollerR = {PWM_ROLLER_R, FWD_ROLLER_R, BWD_ROLLER_R, true, maxSpeed};
-  Motor Intake = {PWM_INTAKE,FWD_INTAKE,BWD_INTAKE,true,maxSpeed};
-  Motor Index = {PWM_INDEX, FWD_INDEX, BWD_INDEX, true, maxSpeed};
-  
-  setupMotor(FR);
-  setupMotor(FL);
-  setupMotor(BR);
-  setupMotor(BL);
-  setupMotor(ML);
-  setupMotor(MR);
-  setupMotor(RollerL);
-  setupMotor(RollerR);
-  setupMotor(Intake);
-  setupMotor(Index);
-  moveMotor(Intake,250);
-  moveMotor(Index,250);
-  moveMotor(RollerL,250);
-  moveMotor(RollerR,250);
-  moveMotor(FL,250);
-  moveMotor(FR,250);
-  moveMotor(ML,250);
-  moveMotor(MR,250); 
-  moveMotor(BL,250);
-  moveMotor(BR,250);
-  delay(5);  
+  //note Ibus Lib reads switches from 0 - 9. Thus offset.
+  int y = controllerMap(readChannel(1),250);
+  int x = controllerMap(readChannel(3),200);
+  int servo = readSwitch(4);
+  int auton = readSwitch(6);
+  int intake_dir = readSwitch(7);
+  int intake = readSwitch(5);
+
+  if (auton == 0) { 
+    
+  } else if (auton == 1) {
+   //driver control  
+   FL.move(y+x);
+   FR.move(y-x);
+   BL.move(y+x);
+   BR.move(y-x);
+
+   if (intake == 1){
+    if (intake_dir == 1) roller(250);
+    else if (intake_dir == 0) roller(-250);
+    else if (intake_dir == -1) roller(0);
+   } else {
+    roller(0);
+   }
+   
+   if (servo) {
+    indexer.write(0);
+   } else {
+    indexer.write(180);
+   }
+   
+  } 
+
+
+  delay(2); 
 }
